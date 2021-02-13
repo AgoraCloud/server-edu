06cfbdd8f474c100b1b80b42d8b3a01bc79c4d04
Said Ghamra <s.ghamra@hotmail.com>
2021-02-13 18:25:03 -0500

    #32 Implemented wiki page dto's

--- src/modules/wiki/pages/dto/create-page.dto.ts
+++ src/modules/wiki/pages/dto/create-page.dto.ts
@@ -1,1 +1,12 @@
-export class CreateWikiPageDto {}
+import { IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class CreateWikiPageDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(1)
+  title: string;
+
+  @IsString()
+  @IsNotEmpty()
+  body: string;
+}
--- src/modules/wiki/pages/dto/page.dto.ts
+++ src/modules/wiki/pages/dto/page.dto.ts
@@ -1,1 +1,25 @@
-export class WikiPageDto {}
+import { WorkspaceDto } from './../../../workspaces/dto/workspace.dto';
+import { UserDto } from './../../../users/dto/user.dto';
+import { WikiSectionDto } from './../../sections/dto/section.dto';
+import { Exclude, Expose } from 'class-transformer';
+
+@Exclude()
+export class WikiPageDto {
+  @Expose()
+  readonly id: string;
+
+  @Expose()
+  readonly title: string;
+
+  @Expose()
+  readonly body: string;
+
+  @Expose()
+  readonly workspace: WorkspaceDto;
+
+  @Expose()
+  readonly user: UserDto;
+
+  @Expose()
+  readonly section: WikiSectionDto;
+}
